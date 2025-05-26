e0fa1b84fa13a9cf8d12df1934f9a7e9be778781
Dustin Kim <dustin.kim@cupix.com>
2025-05-26 10:20:39 +0900

    build: fix issues regarding to embedding

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,7 +1,16 @@
-set(cp_root_dir "${CMAKE_SOURCE_DIR}")
-set(cp_include_dir "${cp_root_dir}/include")
+function(cp_executable_settings target_name folder_name)
+    set_target_properties(${target_name} PROPERTIES FOLDER ${folder_name})
+    set_target_properties(${target_name} PROPERTIES LINKER_LANGUAGE CXX)
 
-function(cp_common_settings target_name folder_name)
+    file(GLOB_RECURSE src_files CONFIGURE_DEPENDS
+        ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp ${CMAKE_CURRENT_SOURCE_DIR}/*.h)
+
+    source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR} PREFIX src FILES ${src_files})
+endfunction()
+
+function(cp_library_settings target_name folder_name project_root_dir)
+    set(cp_include_dir "${project_root_dir}/include")
+
     set_target_properties(${target_name} PROPERTIES FOLDER ${folder_name})
     set_target_properties(${target_name} PROPERTIES LINKER_LANGUAGE CXX)
 
@@ -16,5 +25,10 @@ function(cp_common_settings target_name folder_name)
     endif()
 
     target_compile_definitions(${target_name} PRIVATE "${target_name}_EXPORTS")
-    target_include_directories(${target_name} PUBLIC ${cp_include_dir})
+    target_include_directories(${target_name} PUBLIC
+        $<BUILD_INTERFACE:${cp_include_dir}>
+        $<INSTALL_INTERFACE:include>
+    )
+
+    install(TARGETS ${target_name} EXPORT skat-coreTargets)
 endfunction()
